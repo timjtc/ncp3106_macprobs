5e55032af5f6bb19ef49fa634e2f7b918783299a
PTCYG\timothyjtc <concepciontimothy@gmail.com>
2023-10-11 16:31:19 +0800

    worked on mp5prob4

--- src/main/java/com/uecpe20231122784/macprob5/macprob5_problem4.java
+++ src/main/java/com/uecpe20231122784/macprob5/macprob5_problem4.java
@@ -4,8 +4,14 @@ import com.uecpe20231122784.lib.print;
 
 public class macprob5_problem4 {
     public static void main(String[] args) {
+        
+        final int TOSS_ITERATIION = 20;
+        
         Coin c = new Coin();
         print.ln("Side facing up: " + c.getSideUp());
-        
+
+        for (int i = 0; i < TOSS_ITERATIION; i++) {
+            print.ln("Toss#" + i + ": " + c.getSideUp());
+        }                
     }
 }
